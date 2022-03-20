a6a4c12e9d00e73c83c069020d57046dd9a3a39d
cat <marvinarivera@gmail.com>
2022-03-20 13:51:07 -0400

    first git

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import './Home.css';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome To The Home Page</h1>
+            <Link to="PathOne" className="PathOneLink">Path One</Link>
+            <Link to="PathTwo" className="PathTwoLink">Path Two</Link>
+            <Link to="PathThree" className="PathThreeLink">Path Three</Link>
+        </div>
+    )
+}
+
+export default Home;
